5234ba1488262600c15c7bf8057d3dcf5416bdd9
Moniruzzaman Monir <imonir.com@gmail.com>
2018-11-18 12:48:30 +0600

    deploy fixes

--- deploy.py
+++ deploy.py
@@ -10,12 +10,12 @@ environment = sys.argv[2]
 
 
 print(f"Down environement: {environment}")
-call(['python', '-c', f"./run.py down ${branch}"])
+call(['bash', '-c', f"./run.py down {branch}"])
 
 print(f"Getting Latest code from {branch}")
-call(['bash', '-c', f"git pull origin ${branch}"])
+call(['bash', '-c', f"git pull origin {branch}"])
 call(['bash', '-c' , 'git submodule update --recursive --remote'])
 
 print(f"Up environment: {environment}")
-call(['python', '-c', f"./run.py up ${branch}"])
+call(['bash', '-c', f"./run.py up {branch}"])
 
