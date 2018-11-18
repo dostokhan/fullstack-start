eeeac6b15854655a89ca98321dc6f541f8e24297
Moniruzzaman Monir <imonir.com@gmail.com>
2018-11-18 12:37:31 +0600

    submodule pull cmd added

--- deploy.py
+++ deploy.py
@@ -14,6 +14,7 @@ call(['python', '-c', f"./run.py down ${branch}"])
 
 print(f"Getting Latest code from {branch}")
 call(['bash', '-c', f"git pull origin ${branch}"])
+call(['bash', '-c' , 'git submodule update --recursive --remote'])
 
 print(f"Up environment: {environment}")
 call(['python', '-c', f"./run.py up ${branch}"])
